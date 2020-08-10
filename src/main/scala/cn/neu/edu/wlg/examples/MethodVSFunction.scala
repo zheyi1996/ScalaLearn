dff7762c9a1a843a4f57eebcc698450b57ef2dbe
WeiLiuGuo <305616809@qq.com>
2020-08-10 09:03:06 +0800

    2020-8-10

---
+++ src/main/scala/cn/neu/edu/wlg/examples/MethodVSFunction.scala
@@ -0,0 +1,14 @@
+package cn.neu.edu.wlg.examples
+
+object MethodVSFunction {
+
+  def main(args: Array[String]): Unit = {
+    // 使用
+  }
+}
+class Dog {
+
+  def sum(n1: Int, n2: Int): Int = {
+    n1 + n2
+  }
+}
